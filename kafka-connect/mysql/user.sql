4496d56cae4b9898150a3289a17eafbfbcb5b483
quangpn24 <19522100@gm.uit.edu.vn>
2024-06-15 15:10:13 +0700

    Update table

--- kafka-connect/mysql/user.sql
+++ kafka-connect/mysql/user.sql
@@ -2,6 +2,7 @@ CREATE TABLE users (
    id int AUTO_INCREMENT PRIMARY KEY,
    username varchar(256),
    age integer,
+   gender varchar(256),
    created_at timestamp DEFAULT CURRENT_TIMESTAMP,
    updated_at timestamp DEFAULT CURRENT_TIMESTAMP
 );
